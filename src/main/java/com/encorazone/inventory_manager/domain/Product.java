5453e4aef72ac0b2e590a8625f604b046d0d9aae
Leonardo Trevizo <leonardo.trevizo@encora.com>
2025-06-20 12:44:57 -0600

    Deleted getters & setters
    Modified Product.java:
     - Deleted getters & setters
     - Enabled lombok dependency to keep the code clean
     - ...Next project will use ID instead of UUID

--- src/main/java/com/encorazone/inventory_manager/domain/Product.java
+++ src/main/java/com/encorazone/inventory_manager/domain/Product.java
@@ -1,12 +1,14 @@
 package com.encorazone.inventory_manager.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Data
 @Entity
 @Table(name = "PRODUCTS")
 public class Product {
@@ -48,67 +50,5 @@ public class Product {
         updateDate = LocalDateTime.now();
     }
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(BigDecimal unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public Integer getStockQuantity() {
-        return stockQuantity;
-    }
-
-    public void setStockQuantity(Integer stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public LocalDateTime getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(LocalDateTime updateDate) {
-        this.updateDate = updateDate;
-    }
 }
